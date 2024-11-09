3273c6b07ba85822cea39f9ffa80a64ba403985e
Deniel Angelo Bare <denielangelobare@gmail.com>
2024-11-09 18:25:44 +0800

    update withLogin

---
+++ pages/instructors.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <a-table :dataSource="instructors" :columns="columns"></a-table>
+    </div>
+</template>
+
+<script>
+export default {
+data(){
+    return {
+    instructors: [],
+    columns: [
+        {
+        title: 'First Name',
+        dataIndex: 'firstName',
+        key: 'firstName',
+        },
+        {
+        title: 'Last Name',
+        dataIndex: 'lastName',
+        key: 'lastName',
+        },
+        {
+        title: 'Subject',
+        dataIndex: 'subject',
+        key: 'subject',
+        }
+    ]
+    }
+},
+async fetch(){
+    let {data} = await this.$axios.get('/instructors');
+    this.instructors = data;
+    console.log('instructors :>> ', this.instructors);
+},
+methods: {
+},
+created(){
+    if (this.$auth.loggedIn) {
+    this.username = this.$auth.user.username
+    this.name = this.$auth.user.name
+    this.email = this.$auth.user.email
+    }
+}
+}
+</script>
